7a07b9d27be2f8fdc205d292b6daafa24b628fae
snatcherke <159681638+snatcherke@users.noreply.github.com>
2024-02-27 11:46:29 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,17 @@
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Ask user for their age
+age = input("Enter your age: ")
+
+# Ask user for their location
+location = input("Enter your location: ")
+
+# Ask user for their phone number
+phonenumber = input("Enter your phone number: ")
+
+# Ask user their hobby
+hobby = input("Enter your hobby: ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}, your phone number is {phonenumber} and your hobby is {hobby}.")
